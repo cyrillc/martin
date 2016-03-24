67f2dfeb5c6704d39b5561277511f6373e09bb82
grafhan2 <grafhan2@students.zhaw.ch>
2016-03-24 19:02:25 +0100

    MySQL Hack etwas sauberer gemacht - Funktionstest war positiv.

--- config/vagrant_config/mysql/init_mysql.sh
+++ config/vagrant_config/mysql/init_mysql.sh
@@ -9,10 +9,15 @@ sudo mv /etc/mysql/my.cnf /etc/mysql/my.cnf.backup
 # Load our config
 sudo cp /vagrant/config/vagrant_config/mysql/my.cnf /etc/mysql/my.cnf
 
+# Reboot Network Service
+sudo /etc/init.d/networking stop
+sudo /etc/init.d/networking start
 
 # Start MySQL Service on Server
 sudo /etc/init.d/mysql start
 
-# The Hack just works with a Restart at the end
+# Anders funktioniert es nicht - sorry
+sudo /etc/init.d/mysql stop
+sudo initclt --system stop mysql
+sudo /etc/init.d/mysql start
 
-sudo reboot
