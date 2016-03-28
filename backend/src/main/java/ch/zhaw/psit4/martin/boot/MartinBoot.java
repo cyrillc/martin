80c5d86556200c844c207f2d2b6ac1fe6725ad6c
Daniel Fabian <jtek.fdlt@gmail.com>
2016-03-28 15:25:16 +0200

    Merge in dev

---
+++ backend/src/main/java/ch/zhaw/psit4/martin/boot/MartinBoot.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package ch.zhaw.psit4.martin.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ch.zhaw.psit4.martin.frontend.FrontendController;
+import ch.zhaw.psit4.martin.frontend.IFrontendController;
+import ch.zhaw.psit4.martin.modulelib.IPluginLibrary;
+import ch.zhaw.psit4.martin.modulelib.PluginLibraryBootstrap;
+
+/**
+ * Entry point for the application to bootstrap jpf, SPRING and invoke
+ * <code>ModuleLibraryBootstrap.boot()</code>.
+ *
+ * @author Daniel Fabian
+ * @version 0.0.1-SNAPSHOT
+ */
+@SpringBootApplication
+public class MartinBoot {
+
+    /*
+     * The martin library singleton
+     */
+    private static IPluginLibrary library;
+    /*
+     * The frontend controller singleton
+     */
+    private static IFrontendController frontendController;
+
+    /**
+     * Main application entry point launches MArtIn and used components.
+     * 
+     * @param args
+     *            Command line arguments (unused)
+     */
+    public static void main(String[] args) {
+        // boot Spring
+        SpringApplication.run(MartinBoot.class, args);
+        // boot library
+        library = (new PluginLibraryBootstrap()).boot();
+        // boot frontend controller
+        frontendController = (IFrontendController) new FrontendController();
+        frontendController.start();
+        // TODO: Boot other components
+    }
+
+    public static IPluginLibrary getPluginLibrary() {
+        return library;
+    }
+    
+    public static IFrontendController getFrontendController() {
+        return frontendController;
+    }
+}
---
+++ backend/src/main/java/ch/zhaw/psit4/martin/frontend/Command.java
@@ -0,0 +1,15 @@
+package ch.zhaw.psit4.martin.frontend;
+
+public class Command {
+
+    private final String content;
+
+    public Command(String content) {
+        this.content = content;
+    }
+
+
+    public String getContent() {
+        return content;
+    }
+}
