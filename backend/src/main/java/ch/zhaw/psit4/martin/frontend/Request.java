93be2715746e66c9e6a897d7c5bcfd954895a913
moretgio <moretgio@students.zhaw.ch>
2016-03-31 09:01:27 +0200

    Reqest object

---
+++ backend/src/main/java/ch/zhaw/psit4/martin/frontend/Request.java
@@ -0,0 +1,14 @@
+package ch.zhaw.psit4.martin.frontend;
+
+public class Request {
+    
+    private String command;
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+}
