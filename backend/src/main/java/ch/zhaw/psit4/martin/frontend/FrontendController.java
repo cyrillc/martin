41d3a8e2cd9741c4bd96536dd30f32b1736c9542
moretgio <moretgio@students.zhaw.ch>
2016-04-03 17:36:30 +0200

    javadoc

--- backend/src/main/java/ch/zhaw/psit4/martin/frontend/FrontendController.java
+++ backend/src/main/java/ch/zhaw/psit4/martin/frontend/FrontendController.java
@@ -10,6 +10,12 @@ import ch.zhaw.psit4.martin.common.Response;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+/**
+ * This class connect the Frontend with the AI using REST.
+ * 
+ * @version 0.0.1-SNAPSHOT
+ *
+ */
 @RestController
 public class FrontendController implements IFrontendController {
 
@@ -26,6 +32,13 @@ public class FrontendController implements IFrontendController {
         // TODO: initialize
     }
 
+    /**
+     * This method receive via a GET Request a command to forward to the AI. The
+     * AI will give back a Response a this will be sent to Frontend.
+     * 
+     * @param command
+     * @return the response of the AI
+     */
     @CrossOrigin(origins = { "http://localhost:4141",
             "http://srv-lab-t-825:4141" })
     @RequestMapping("/command")
