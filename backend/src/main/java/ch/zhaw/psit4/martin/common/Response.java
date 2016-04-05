0168a36d78c0ae65c5f6bb89c79346b1a052f0de
moretgio <moretgio@students.zhaw.ch>
2016-04-05 21:35:23 +0200

    ORM annotations

--- backend/src/main/java/ch/zhaw/psit4/martin/common/Response.java
+++ backend/src/main/java/ch/zhaw/psit4/martin/common/Response.java
@@ -1,13 +1,29 @@
 package ch.zhaw.psit4.martin.common;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "response")
 public class Response {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "content")
+    @NotNull
     private final String content;
 
     public Response(String content) {
         this.content = content;
     }
-
 
     public String getContent() {
         return content;
