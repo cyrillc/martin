0861254a5630bb2ca836720d83021a8c575475ff
moretgio <moretgio@students.zhaw.ch>
2016-04-06 22:05:47 +0200

    HistoryItemService implemented

---
+++ backend/src/main/java/ch/zhaw/psit4/martin/common/service/HistoryItemService.java
@@ -0,0 +1,25 @@
+package ch.zhaw.psit4.martin.common.service;
+
+import org.springframework.stereotype.Service;
+
+import ch.zhaw.psit4.martin.common.HistoryItem;
+import ch.zhaw.psit4.martin.common.dao.HistoryItemDAO;
+
+/**
+ * Service just to test some work.. should waiting for a better solution
+ * 
+ * @version 0.0.1-SNAPSHOT
+ */
+@Service
+public class HistoryItemService {
+    
+    private HistoryItemDAO historyItemDAO;
+
+    public void setHistoryItemDAO(HistoryItemDAO historyItemDAO) {
+        this.historyItemDAO = historyItemDAO;
+    }
+    
+    public void addHistoryItem(HistoryItem historyItem){
+        this.historyItemDAO.add(historyItem);
+    }
+}
