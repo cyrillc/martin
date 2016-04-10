70774a184f7f945450f597ae94b6c7ecbd2f15ea
moretgio <moretgio@students.zhaw.ch>
2016-04-10 16:47:59 +0200

    still no right UnitTest

--- backend/src/test/java/ch/zhaw/psit4/martin/common/service/HistoryItemServiceTest.java
+++ backend/src/test/java/ch/zhaw/psit4/martin/common/service/HistoryItemServiceTest.java
@@ -1,5 +1,10 @@
 package ch.zhaw.psit4.martin.common.service;
 
+import java.util.List;
+
+import javax.validation.constraints.AssertTrue;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -27,5 +32,15 @@ public class HistoryItemServiceTest {
 //        HistoryItem hs = new HistoryItem(request, response);
 //        this.historyItemService.addHistoryItem(hs);
 //    }
+    
+//    @Test
+//    public void testGetHistory(){
+//        List<HistoryItem> history = this.historyItemService.getHistory();
+//        Assert.assertTrue(history.size() > 0);
+//        for(HistoryItem item : history){
+//            System.out.println(item.getRequest().getCommand());
+//        }
+//    }
+    
 
 }
