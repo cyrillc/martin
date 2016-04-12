236046a93fded23e2af73f3c3fed28af73230e09
moretgio <moretgio@students.zhaw.ch>
2016-04-12 22:10:54 +0200

    2 test cases with mock

--- backend/src/test/java/ch/zhaw/psit4/martin/common/service/HistoryItemServiceTest.java
+++ backend/src/test/java/ch/zhaw/psit4/martin/common/service/HistoryItemServiceTest.java
@@ -2,59 +2,66 @@ package ch.zhaw.psit4.martin.common.service;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
-
-import javax.validation.constraints.AssertTrue;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import ch.zhaw.psit4.martin.common.HistoryItem;
-import ch.zhaw.psit4.martin.common.LiquibaseTestFramework;
 import ch.zhaw.psit4.martin.common.Request;
 import ch.zhaw.psit4.martin.common.Response;
+import ch.zhaw.psit4.martin.common.dao.HistoryItemDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:Beans.xml", "classpath:Beans-unit-tests.xml"})
+@ContextConfiguration({ "classpath:Beans.xml",
+        "classpath:Beans-unit-tests.xml" })
 public class HistoryItemServiceTest {
-	@Autowired
+
+    class DaoStub extends HistoryItemDAO {
+        public boolean addIsCalled = false;
+        public boolean getAllIsCalled = false;
+
+        public void add(HistoryItem historyItem) {
+            this.addIsCalled = true;
+        }
+
+        public List<HistoryItem> getAll() {
+            this.getAllIsCalled = true;
+            return new ArrayList<>();
+        }
+    }
+
+    @Autowired
     private HistoryItemService historyItemService;
-	
-	@Autowired
-	private LiquibaseTestFramework liquibase;
+    private DaoStub daoStub;
 
     @Before
     public void setUp() {
-        liquibase.createDatabase("database/db.changeset-schema-latest.xml");
+        daoStub = new DaoStub();
+        historyItemService.setHistoryItemDAO(daoStub);
     }
 
-   @Test
-   @Transactional
-    public void testAdd() {
+    @Test
+    public void addHistoryItemUsingDAO() {
+
         Request request = new Request("testRequest");
         Response response = new Response("testResponse");
         HistoryItem hs = new HistoryItem(request, response);
         historyItemService.addHistoryItem(hs);
-        
-        assertEquals(historyItemService.getHistory().size(), 1);
+
+        assertTrue(this.daoStub.addIsCalled);
     }
-    
-    //@Test
-    //public void testGetHistory(){
-    //    List<HistoryItem> history = this.historyItemService.getHistory();
-    //    Assert.assertTrue(history.size() > 0);
-    //    for(HistoryItem item : history){
-    //        System.out.println(item.getRequest().getCommand());
-    //    }
-    //}
-    
+
+    @Test
+    public void canGetAHistoryItemListUsingDAO() {
+        this.historyItemService.getHistory();
+        Assert.assertTrue(this.daoStub.getAllIsCalled);
+    }
 
 }
