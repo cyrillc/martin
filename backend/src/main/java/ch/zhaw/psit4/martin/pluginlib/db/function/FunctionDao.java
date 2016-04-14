3b059a121b58c7e3ca6fa103cfef0c0cd2fee44a
Fabian Camenzind <camenfa1@students.zhaw.ch>
2016-04-14 09:05:17 +0200

    FunktionDAO hinzugefügt
    Bisher noch ohne Test

---
+++ backend/src/main/java/ch/zhaw/psit4/martin/pluginlib/db/function/FunctionDao.java
@@ -0,0 +1,57 @@
+package ch.zhaw.psit4.martin.pluginlib.db.function;
+
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+/**
+ * DAO for Paramter to manage database access. The class defines the methods
+ * to access the plugin database.
+ * 
+ * 
+ * @version 0.0.1-SNAPSHOT
+ */
+@Repository
+public class FunctionDao {
+    
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sf) {
+        this.sessionFactory = sf;
+    }
+
+    public void add(Function function) {
+        Session session = this.sessionFactory.getCurrentSession();
+        session.persist(function);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<Function> listFunctions() {
+        Session session = this.sessionFactory.getCurrentSession();
+        return session.createQuery("from Function").list();
+    }
+    
+    public Function getById(int id) {
+        Session session = this.sessionFactory.getCurrentSession();
+        return (Function) session.get(Function.class,id);
+    }
+    
+    /**
+     * USE WITH CAUTION!!
+     * @param function
+     */
+    public void updateFunction(Function function) {
+        Session session = this.sessionFactory.getCurrentSession();
+        session.update(function);
+    }
+    
+    public void removeFunction(int id) {
+        Session session = this.sessionFactory.getCurrentSession();
+        Function hm = this.getById(id);
+        if (hm != null) {
+            session.delete(hm);
+        }
+    }
+    
+}
---
+++ backend/src/main/java/ch/zhaw/psit4/martin/pluginlib/db/function/FunctionService.java
@@ -0,0 +1,57 @@
+package ch.zhaw.psit4.martin.pluginlib.db.function;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ch.zhaw.psit4.martin.pluginlib.db.keyword.Keyword;
+import ch.zhaw.psit4.martin.pluginlib.db.keyword.KeywordDao;
+
+/**
+* This class provides methods to easily save and read Function from the
+* database.
+* 
+* @version 0.0.1-SNAPSHOT
+*/
+@Service
+public class FunctionService {
+
+   private FunctionDao functionDao;
+   
+   public void setFunctionDao(FunctionDao functionDao) {
+       this.functionDao = functionDao;
+   }
+
+   @Transactional
+   public void addFunction(Function function) {
+       this.functionDao.add(function);
+   }
+
+   @Transactional
+   public List<Function> listFunctions() {
+       return this.functionDao.listFunctions();
+   }
+
+   @Transactional
+   public Function getFunctionById(int id) {
+       return functionDao.getById(id);
+   }
+
+   /**
+    * USE WITH CAUTION!!
+    * @param function
+    */
+   @Transactional
+   public void updateFunction(Function function) {
+       this.functionDao.updateFunction(function);
+   }
+
+   @Transactional
+   public void removeFunction(int id) {
+       this.functionDao.removeFunction(id);
+   }
+
+
+
+}
