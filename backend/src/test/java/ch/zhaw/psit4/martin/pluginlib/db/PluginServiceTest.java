29e52f32f2a1ec6fe942eaf1477f5654f1b46620
Fabian Camenzind <camenfa1@students.zhaw.ch>
2016-04-21 15:49:08 +0200

    angefangen den PluginService Test zu schreiben

---
+++ backend/src/test/java/ch/zhaw/psit4/martin/pluginlib/db/PluginServiceTest.java
@@ -0,0 +1,79 @@
+package ch.zhaw.psit4.martin.pluginlib.db;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import ch.zhaw.psit4.martin.common.LiquibaseTestFramework;
+import ch.zhaw.psit4.martin.pluginlib.db.parameter.ParameterService;
+import ch.zhaw.psit4.martin.pluginlib.db.plugin.Plugin;
+import ch.zhaw.psit4.martin.pluginlib.db.plugin.PluginService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:Beans.xml", "classpath:Beans-unit-tests.xml"})
+public class PluginServiceTest {
+
+	@Autowired
+	private LiquibaseTestFramework liquibase;
+
+	@Autowired
+    private PluginService pluginService;
+    private Log log;
+
+    @Before
+    public void setUp(){
+    	liquibase.createDatabase("database/unit-tests/pluginTest/db.pluginUnitTest-1.0.xml");
+        log = LogFactory.getLog(PluginServiceTest.class);        
+    }
+
+    @Test
+    public void testListplugins() throws Exception {
+        List<Plugin> plugins = pluginService.listPlugins();
+        plugins.stream().forEach(plugin -> printPlugin(plugin));
+        assertEquals(false,plugins.isEmpty());
+        assertEquals(4,plugins.size());
+
+    }
+
+    private void printPlugin(Plugin plugin) {
+        StringBuilder str  = new StringBuilder(plugin.getId()));
+        log.info(plugin.getName()+" has ID "+ plugin.getId());
+    }
+
+    private void printParam(plugin param) {
+        log.info(param.getId()+", "+param.getName()+", "+param.getType());
+        log.info("Keywords for "+param.getName());
+        param.getKeywords().stream().forEach(keyword -> log.info(keyword.getKeyword()));
+    }
+    /*
+    @Test
+    public void testAddPlugin() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testListPlugins() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetPluginById() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testRemovePlugin() {
+        fail("Not yet implemented");
+    }
+
+    */
+}
