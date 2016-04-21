336ed1e7ee9a8fb8aa22941c38a2a9399f3bb8a9
grafhan2 <grafhan2@students.zhaw.ch>
2016-04-21 18:43:06 +0200

    Plugin_list.js dokumentiert und Dokumentationen erstellt

--- frontend/homepage/js/admin/plugin_list.js
+++ frontend/homepage/js/admin/plugin_list.js
@@ -1,14 +1,19 @@
+// Renderobject for Pluginlists
 function PluginListRenderer(pluginList) {
     this.pluginList = pluginList;
 }
+
 
 PluginListRenderer.prototype.renderPlugins = function () {
     this.pluginList.forEach(function(element) {
         var pluginName = $('<p class="pluginName"></p>');
         var pluginDescription = $('<p class="pluginDescription"></p>');
 
+        //render Plugin-Name
         pluginName.append(element.name);
+        //render Plugin-Description
         pluginDescription.append(element.description);
+        //to render the whole List, we need to call an extern Renderer
         functionNames = createFunctionNameList(element.functionInformation);
 
 
@@ -16,6 +21,7 @@ PluginListRenderer.prototype.renderPlugins = function () {
     });
 };
 
+// Function iterates through a whole list of Functions and Renders it in a li Element
 var createFunctionNameList = function (functionList) {
     var htmlFunctionList = $('<ul class="functionNames"></ul>');
 
