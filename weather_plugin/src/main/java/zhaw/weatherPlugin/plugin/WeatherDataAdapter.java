4878d6aac404df296ddb60b0fa4047056214c183
moretgio <moretgio@students.zhaw.ch>
2016-04-27 18:20:56 +0200

    givenADateCanSearchTheClosestForecast implemented

--- weather_plugin/src/main/java/zhaw/weatherPlugin/plugin/WeatherDataAdapter.java
+++ weather_plugin/src/main/java/zhaw/weatherPlugin/plugin/WeatherDataAdapter.java
@@ -5,31 +5,35 @@ import java.util.Date;
 import org.bitpipeline.lib.owm.WeatherData;
 
 public class WeatherDataAdapter {
-    private WeatherData data;
+    private WeatherData owmData;
 
     public WeatherDataAdapter(WeatherData data) {
-        this.data = data;
+        this.owmData = data;
+    }
+    
+    public WeatherData getOwmData() {
+        return owmData;
     }
 
     public boolean hasRain() {
-        return this.data.hasRain();
+        return this.owmData.hasRain();
     }
 
     public Date getDate() {
         //owm returns date as a unix timestamp
-        return new Date(this.data.getDateTime() * 1000);
+        return new Date(this.owmData.getDateTime() * 1000);
     }
 
     public int getRain() {
-        return this.hasRain() ? this.data.getRain() : 0;
+        return this.hasRain() ? this.owmData.getRain() : 0;
     }
 
     public String getWeatherDescription() {
-        return this.data.getWeatherConditions().get(0).getDescription();
+        return this.owmData.getWeatherConditions().get(0).getDescription();
     }
 
     public float getTemperature() {
-        return convertKelvinToCelsius(this.data.getTemp());
+        return convertKelvinToCelsius(this.owmData.getTemp());
     }
 
     public float convertKelvinToCelsius(float kelvin) {
