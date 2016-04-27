4704d0f60d0758aa1e6a56b6def963dacb99277a
covolmar <covolmar@students.zhaw.ch>
2016-04-27 22:39:07 +0200

    completed unit tests

--- backend/src/test/java/ch/zhaw/psit4/martin/db/PluginServiceTest.java
+++ backend/src/test/java/ch/zhaw/psit4/martin/db/PluginServiceTest.java
@@ -2,6 +2,8 @@ package ch.zhaw.psit4.martin.db;
 
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
@@ -15,6 +17,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import ch.zhaw.psit4.martin.common.LiquibaseTestFramework;
 import ch.zhaw.psit4.martin.db.author.Author;
+import ch.zhaw.psit4.martin.db.author.AuthorService;
+import ch.zhaw.psit4.martin.db.function.Function;
+import ch.zhaw.psit4.martin.db.function.FunctionService;
+import ch.zhaw.psit4.martin.db.parameter.ParameterService;
 import ch.zhaw.psit4.martin.db.plugin.Plugin;
 import ch.zhaw.psit4.martin.db.plugin.PluginService;
 
@@ -27,6 +33,15 @@ public class PluginServiceTest {
 
 	@Autowired
     private PluginService pluginService;
+	
+	@Autowired
+	private AuthorService authorService;
+	
+	@Autowired
+	private FunctionService functionService;
+	
+	@Autowired
+    private ParameterService parameterService;
     private Log log;
 
     @Before
@@ -58,44 +73,55 @@ public class PluginServiceTest {
         log.info(str);
     }
 
-    
-    @Test
-    public void testAddPlugin() {
-        Plugin newPlugin = new Plugin(3);
-        Author author = new Author(3);
-        author.setName("TestAuthor");
-        newPlugin.setAuthor(author);
-        newPlugin.setDescription("Test Description");
-        newPlugin.setName("Testname");
-        
-        pluginService.addPlugin(newPlugin);
-        
-        Plugin plugin = pluginService.getPluginById(3);
-        assertNotNull(plugin);
-        assertEquals(3,plugin.getId());
-        assertEquals("Test Description",plugin.getDescription());
-        assertEquals("Testname",plugin.getName());
-        assertEquals(3,plugin.getAuthor().getId());
-        
-    }
-
    
-
     @Test
     public void testGetPluginById() {
-        Plugin plugin = pluginService.getPluginById(2);
+        Plugin plugin = pluginService.getPluginById(1);
         assertNotNull(plugin);
-        assertEquals(2,plugin.getId());
+        assertEquals(1,plugin.getId());
         assertEquals("Tells the weather for a given time and location",plugin.getDescription());
         assertEquals("WeatherApp",plugin.getName());
         assertEquals(1,plugin.getAuthor().getId());
         
     }
+    
+    @Test
+    public void testAddPlugin() {
+        Plugin newPlugin = createPlugin();
+        pluginService.addPlugin(newPlugin);
+        
+        int pluginID = newPlugin.getId();      
+        Plugin plugin = pluginService.getPluginById(pluginID);
+        
+        assertNotNull(plugin);
+        assertEquals(pluginID,plugin.getId());
+        assertEquals("Test Description",plugin.getDescription());
+        assertEquals("Testname",plugin.getName());
+        
+    }
+    
+    private Plugin createPlugin() {  
+        Plugin newPlugin = new Plugin();
+        Author author = new Author();
+        author.setName("TestAuthor");
+        authorService.addAuthor(author);
+        
+        newPlugin.setAuthor(author);
+        newPlugin.setDate(new java.sql.Date(Calendar.getInstance().getTime().getTime()));
+        newPlugin.setDescription("Test Description");
+        newPlugin.setName("Testname");
+        return newPlugin;      
+    }
 
+  
     @Test
     public void testRemovePlugin() {
         pluginService.removePlugin(1);
+        
         assertNull(pluginService.getPluginById(1));
+        assertNull(functionService.getFunctionById(1));
+        assertNull(parameterService.getParameterById(1));
+        assertNotNull(authorService.getAuthorById(1));
     }
 
     
