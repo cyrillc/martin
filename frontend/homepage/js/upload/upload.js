8903c335f59720f5c835ef140baacebaf2c4fa3e
covolmar <covolmar@students.zhaw.ch>
2016-05-04 20:15:35 +0200

    upload skript

--- frontend/homepage/js/upload/upload.js
+++ frontend/homepage/js/upload/upload.js
@@ -1,14 +1,28 @@
 
-$(document).ready(function () {
-   
-    fs.readFile(req.files.file.path, function (err, data) {
-  // ...
-  var newPath = __dirname + "/uploads/uploadedFileName";
-  fs.writeFile(newPath, data, function (err) {
-    res.redirect("back");
-  });
+$(document).ready(function () { 
+
+$('#upload').on('click', function() {
+    var file_data = $('#jar').prop('files')[0];   
+    var form_data = new FormData();                  
+    form_data.append('file', file_data);
+    alert(form_data);                             
+    $.ajax({
+                url: '/upload', // point to server-side PHP script 
+                dataType: 'text',  // what to expect back from the PHP script, if anything
+                cache: false,
+                contentType: false,
+                processData: false,
+                data: form_data,                         
+                type: 'post',
+                success: function(php_script_response){
+                    alert(php_script_response); // display response from the PHP script, if any
+                }
+     });
 });
 
-    
-    
+
+
+
 });
+
+   
