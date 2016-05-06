9796bd453da2760efd20a042454a8b3fe3c52569
moretgio <moretgio@students.zhaw.ch>
2016-05-06 13:28:59 +0200

    post works

--- frontend/homepage/js/upload/upload.js
+++ frontend/homepage/js/upload/upload.js
@@ -1,40 +1,31 @@
 
 $(document).ready(function () {
     
-// Prepare upload
-    
-// Variable to store your files
-var myfile;
-// Add events
-$('input[type=file]').on('change', prepareUpload);
-// Grab the files and set them to our variable
-function prepareUpload(event) {
-  myfile = event.target.files[0];
-}
-
 
 // upload
-$('form').on('submit', uploadFile);
+$('#upload_btn').on('click', uploadFile);
 
 // Catch the form submit and upload the files
 function uploadFile(event) {
-  event.stopPropagation(); // Stop stuff happening
+	event.stopPropagation(); // Stop stuff happening
     event.preventDefault(); // Totally stop stuff happening
-
-    // START A LOADING SPINNER HERE
-
-    // Create a formdata object and add the files
-    var formdata = new FormData();
-        formdata.append('file', myfile);
+    
+    var formData = new FormData($("#upload_form"));
     
     // create request URL from current URL
     var backendUrl = createRequestURL(frontendUrl, backendPort, "upload");
     
-    //send data to backend
-    $.post(backendUrl,formdata, function (serverResponse) {
+    $.ajax({
+        url: backendUrl,  
+        type: 'POST',
+        data: formData,
+        success:function (serverResponse) {
             $("#response").append("<p>" + serverResponse + "</p>");
-     });
-
+        },
+        cache: false,
+        contentType: false,
+        processData: false
+    });
 }
 });
 
