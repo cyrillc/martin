ae2788dea04bbe28f80888b9c1edadc4946d48f8
zuerrdan <zuerrdan@students.zhaw.ch>
2016-05-11 19:21:31 +0200

    API code-formatierung verschoenert

--- frontend/homepage/js/community/chart_drawer.js
+++ frontend/homepage/js/community/chart_drawer.js
@@ -5,23 +5,7 @@ var createTimingChart = function (chartData) {
     new Chart(canvasDrawer, {
         type: 'pie',
         data: chartData,
-        /*    data: {
-                labels: ["AIController", "SignalProcessor", "I/O", "ModuleLibrary", "PerpherialManager"],
-                datasets: [{
-                    label: '# of Secondes',
-                    data: [12, 19, 3, 5, 2],
-                    backgroundColor: [
-                        "#FF6384",
-                        "#4BC0C0",
-                        "#FFCE56",
-                        "#E7E9ED",
-                        "#36A2EB"
-                    ],
-                }]
-            },*/
-
     });
-
 };
 
 
@@ -32,7 +16,6 @@ TimingChartRenderer.prototype.renderTimingChart = function (timingInfo) {
     var chartData = {
         labels: [],
         datasets: [{
-            //label: '# of milliseconds',
             data: [],
             backgroundColor: [
                 "#FF6384",
