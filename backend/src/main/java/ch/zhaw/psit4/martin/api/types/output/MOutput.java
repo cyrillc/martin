42177a5275b027e0f55225d213e8e41ad40390d0
Fabian Camenzind <camenfa1@students.zhaw.ch>
2016-05-17 17:38:06 +0200

    MOutputType und MOutput zu API hinzugefügt

---
+++ backend/src/main/java/ch/zhaw/psit4/martin/api/types/output/MOutput.java
@@ -0,0 +1,23 @@
+package ch.zhaw.psit4.martin.api.types.output;
+
+public class MOutput {
+
+    private String value;
+    private MOutputType type;
+
+    public MOutput(MOutputType type, String value) {
+        this.value = value;
+        this.type = type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public MOutputType getType() {
+        return type;
+    }
+
+
+
+}
